cc087cea4fde894b5838d8137dca8619998c1571
John Call <johnsimcall@gmail.com>
2016-04-21 21:05:05 -0600

    Added small script to convert zip to tgz quickly

---
+++ zip_to_tgz.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TMP_DIR=$(mktemp -d)
+
+if [ $# -ne 1 ]
+then
+  echo "You forgot to tell me which zip archive to repackage." ; echo
+  exit 1
+else
+  echo "Extracting zip..."
+  unzip -d $TMP_DIR $1
+  echo "Creating new archive..."
+  tar czvf $1.tgz -C $TMP_DIR .
+fi
+
+rm -rf $TMP_DIR
+
+# BASH string manipulation, remove .zip extension
+#http://stackoverflow.com/questions/11265381/how-to-delete-everything-in-a-string-after-a-specific-character
